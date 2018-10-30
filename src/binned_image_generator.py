c32ec2e85abda3c9d9381ca5ba9ec6854a1c829b
Florian Stellbrink <florian@stellbr.ink>
2018-10-30 21:27:55 +0100

    Refactor lab helper from image generator

--- src/binned_image_generator.py
+++ src/binned_image_generator.py
@@ -1,50 +1,14 @@
 from keras_preprocessing.image import DirectoryIterator
-from numpy import array, zeros, floor, full, ndarray
+from numpy import array, zeros
 from skimage.color import rgb2lab
+
+from src.lab_bin_converter import find_bin, bin_to_index
 
 
 class BinnedImageGenerator(DirectoryIterator):
     """
     Produces bw and discretized images.
     """
-
-    def find_bin(self, a: ndarray, b: ndarray)-> ndarray:
-        """
-        Find bins for a and b components of lab
-        :param a: A component from -128 to 128
-        :param b: B component from -128 to 128
-        :return: Bin from 0 to 313
-        """
-        return (floor((a + 128) / self.grid_space) * self.grid_steps
-                + floor((b + 128) / self.grid_space)).astype(int)
-
-    def build_bins(self):
-        # Create array of all rgb colors
-        rgb_values = zeros((256, 256 * 256, 3))
-        for r in range(256):
-            for g in range(256):
-                for b in range(256):
-                    rgb_values[r, g * 256 + b] = [r / 256, g / 256, b / 256]
-
-        # Convert rgb to lab
-        lab_values = rgb2lab(rgb_values).reshape(-1, 3)
-
-        # Find bins
-        lab_bin_indexes = self.find_bin(lab_values[:, 1], lab_values[:, 2])
-        lab_bins = full((int(lab_bin_indexes.max()) + 1), -1, dtype=int)
-        for index in lab_bin_indexes:
-            lab_bins[index] = 1
-
-        # Assign indexes to bins
-        index = 0
-        for i, is_bin in enumerate(lab_bins):
-            if is_bin == 1:
-                lab_bins[i] = index
-                index = index + 1
-
-        # TODO according to the paper this should be 313, not 260
-        print(index)
-        return lab_bins
 
     def __init__(self, directory, image_data_generator,
                  target_size=(256, 256),
@@ -64,9 +28,6 @@ class BinnedImageGenerator(DirectoryIterator):
                                                    subset,
                                                    interpolation,
                                                    dtype)
-        self.grid_space = 10
-        self.grid_steps = 256 // self.grid_space
-        self.lab_bins = self.build_bins()
 
     def _get_batches_of_transformed_samples(self, index_array):
         batch = super(BinnedImageGenerator, self)._get_batches_of_transformed_samples(index_array)
@@ -80,8 +41,8 @@ class BinnedImageGenerator(DirectoryIterator):
         batch_x *= 1.0 / 100.0
 
         # Discretize other dimensions
-        batch_y_bins = self.find_bin(batch_lab[:, :, :, 1], batch_lab[:, :, :, 2])
-        batch_y_categories = self.lab_bins[batch_y_bins]
+        batch_y_bins = find_bin(batch_lab[:, :, :, 1], batch_lab[:, :, :, 2])
+        batch_y_categories = bin_to_index[batch_y_bins]
         oob_mask_cats = batch_y_categories == -1
         oob_batch = batch[oob_mask_cats]
 
---
+++ src/lab_bin_converter.py
@@ -0,0 +1,65 @@
+from typing import Tuple
+
+from numpy import floor, zeros, ndarray, full
+from skimage.color import rgb2lab
+
+grid_space = 10
+grid_steps = 256 // grid_space
+
+
+def find_bin(a: ndarray, b: ndarray) -> ndarray:
+    """
+    Find bins for a and b components of lab
+    :param a: A component from -128 to 128
+    :param b: B component from -128 to 128
+    :return: Bin from 0 to 313
+    """
+    return (floor((a + 128) / grid_space) * grid_steps
+            + floor((b + 128) / grid_space)).astype(int)
+
+
+def build_bins():
+    # Create array of all rgb colors
+    rgb_values = zeros((256, 256 * 256, 3))
+    for r in range(256):
+        for g in range(256):
+            for b in range(256):
+                rgb_values[r, g * 256 + b] = [r / 256, g / 256, b / 256]
+
+    # Convert rgb to lab
+    lab_values = rgb2lab(rgb_values).reshape(-1, 3)
+
+    # Find bins
+    lab_bin_indexes = find_bin(lab_values[:, 1], lab_values[:, 2])
+    lab_bins = full((int(lab_bin_indexes.max()) + 1), -1, dtype=int)
+    for index in lab_bin_indexes:
+        lab_bins[index] = 1
+
+    # Assign indexes to bins
+    index = 0
+    for i, is_bin in enumerate(lab_bins):
+        if is_bin == 1:
+            lab_bins[i] = index
+            index = index + 1
+
+    # TODO according to the paper this should be 313, not 260
+    print(index)
+    return lab_bins
+
+
+def bin_to_ab(bin: int) -> Tuple[int, int]:
+    # TODO make sure these are in range -128, 128
+    return (bin // grid_steps) * grid_space - 128, (bin % grid_steps) * grid_space - 128
+
+
+def build_reverse_bins():
+    result = zeros((313, 2))
+    for i in range(313):
+        a, b = bin_to_ab(i)
+        result[i] = [a, b]
+
+    return result
+
+
+bin_to_index = build_bins()
+index_to_lab = build_reverse_bins()
