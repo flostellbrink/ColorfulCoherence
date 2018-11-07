e82f365d9b6c12d46f652e269b7e496f2dec15eb
Florian Stellbrink <florian@stellbr.ink>
2018-11-07 04:11:04 +0100

    Tweak remote batch size

--- src/remote_runner.py
+++ src/remote_runner.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Config.data_folder = Path("/scratch/lt2316-h18-resources/coco")
     Config.model_folder = Path("/scratch/gusstefl/model")
     Config.log_folder = Path("/scratch/gusstefl/logs")
-    Config.batch_size = 1
+    Config.batch_size = 4
 
     # Run default training
     train_and_test()
