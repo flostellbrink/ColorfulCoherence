Fix softmax implementation

src/color_regularizer.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from keras.engine import Layer
 
-from src.util.util import softmax_temperature
+from src.util.util import softmax_temperature, softmax
 
 
 class ColorRegularizer(Layer):
@@ -11,8 +11,7 @@ class ColorRegularizer(Layer):
     To avoid optimizations removing a dangling layer, the output is used as loss.
     """
 
-    def __init__(self, temperature=0.1, **kwargs):
-        self.temperature = temperature
+    def __init__(self, **kwargs):
         super(ColorRegularizer, self).__init__(**kwargs)
 
     def call(self, x, mask=None):
@@ -20,20 +19,19 @@ class ColorRegularizer(Layer):
 
         # Flatten and normalize original distribution
         original = tf.reshape(original, (-1, tf.shape(original)[-1]))
-        original = softmax_temperature(original, 1.0)
+        original = softmax(original)
 
         # Flatten and approximately one-hot encode modified distribution
         boosted = tf.reshape(boosted, (-1, tf.shape(boosted)[-1]))
-        boosted = softmax_temperature(boosted, self.temperature)
+        boosted = softmax_temperature(boosted)
 
         # Multiply new encoding with old element wise.
         # This will only have a high output if the one hot selection is likely in the original.
         multiplied = tf.multiply(original, boosted)
 
-        # When we have a complete match each sum is 1, we invert this to get a minimizable loss
-        # Initially I intended to subtract it from 1 so the loss would be in [0,1]. but that causes precision issues.
-        # Instead it is now in [0,-1] where -1 is optimal.
-        loss = tf.reduce_mean(-tf.reduce_sum(multiplied, axis=-1))
+        # When we have a complete match each sum is 1, we invert this to get a minimizable loss.
+        # Correct this value for the assumption that the original is one hot encoded.
+        loss = tf.reduce_mean(1.0 - tf.reduce_sum(multiplied, axis=-1) / tf.reduce_max(original, axis=-1))
 
         return loss
 

src/dist_to_lab.py
@@ -25,7 +25,7 @@ class DistToLab(Layer):
         # Flatten classes into 2D array
         color_classes_flat = tf.reshape(color_classes, (-1, tf.shape(color_classes)[-1]))
         # Apply softmax with low temperature to create approximate one hot encoding
-        color_classes_flat = softmax_temperature(color_classes_flat, 0.1)
+        color_classes_flat = softmax_temperature(color_classes_flat)
 
         # Use matrix multiplication to lookup indices in encoding in color map
         ab_colors = tf.matmul(color_classes_flat, self.color_map)

src/gradient_loss.py
@@ -1,4 +1,3 @@
-import keras.backend as k
 import tensorflow as tf
 from tensorflow.python.ops.image_ops_impl import image_gradients
 
@@ -11,6 +10,7 @@ def get_gradients(y):
     y = y[:, :, :, 1:]
     delta_x, delta_y = image_gradients(y)
     return tf.abs(delta_x) + tf.abs(delta_y)
+
 
 def gradient_loss(yTrue, yPred):
     """

src/util/util.py
@@ -61,12 +61,20 @@ def identity_loss(y_true, y_pred):
     return y_pred
 
 
-def softmax_temperature(logits, temperature):
+def softmax(logits):
     """
-    Runs softmax with temperature on logits
+    Softmax function in last dimension of input.
+    """
+    exp_logits = tf.exp(logits)
+    return exp_logits / tf.reshape(tf.reduce_sum(exp_logits, axis=-1), (-1, 1))
+
+
+def softmax_temperature(logits, temperature = 0.01):
+    """
+    Runs softmax with temperature on logits, adds additional hacks to avoid undefinedness of log around 0.
     :param logits: Input distribution
     :param temperature: Temperature. 1 means softmax, towards 0 behaves like one hot
     :return: Softmaxed distribution
     """
-    log_by_temp = tf.log(tf.sigmoid(logits) + Config.epsilon) / temperature
-    return tf.exp(log_by_temp) / tf.reduce_sum(tf.exp(log_by_temp))
+    exp_log_by_temp = tf.exp(tf.log(tf.sigmoid(logits) + Config.epsilon) / temperature)
+    return exp_log_by_temp / tf.reshape(tf.reduce_sum(exp_log_by_temp, axis=-1), (-1, 1))