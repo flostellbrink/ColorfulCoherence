a37748c35c8d33b6e1b3f9f6ba98f49b925f1dda
Florian Stellbrink <florian@stellbr.ink>
2018-11-11 00:48:10 +0100

    Show more examples on validation

--- src/validator.py
+++ src/validator.py
@@ -17,6 +17,28 @@ print("Disabling GPU")
 environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 
+def show_predictions(generator, model):
+    batch_x, batch_y = next(generator)
+    prediction = model.predict(batch_x, batch_size=Config.batch_size)
+
+    for batch_id in range(Config.batch_size):
+        fig, axes = plt.subplots(ncols=4, figsize=(8, 2))
+
+        axes[0].set_title("Original")
+        axes[0].imshow(lab2rgb(batch_y['lab_coherent'][batch_id]))
+
+        axes[1].set_title("Grayscale")
+        axes[1].imshow(squeeze(batch_x[batch_id]), cmap='gray')
+
+        axes[2].set_title("Colorful Prediction")
+        axes[2].imshow(lab2rgb(prediction[0][batch_id]))
+
+        axes[3].set_title("Coherent Prediction")
+        axes[3].imshow(lab2rgb(prediction[1][batch_id]))
+
+        plt.show()
+
+
 def validate(modelfile):
     """
     Train and test in default environment
@@ -39,25 +61,8 @@ def validate(modelfile):
     # Create new model to output the lab-color layers
     model = Model(inputs=model.input, outputs=[lab_colorful, lab_coherent])
 
-    batch_x, batch_y = next(test_generator)
-    prediction = model.predict(batch_x, batch_size=Config.batch_size)
-
-    for batch_id in range(Config.batch_size):
-        fig, axes = plt.subplots(ncols=4, figsize=(8, 2))
-
-        axes[0].set_title("Original")
-        axes[0].imshow(lab2rgb(batch_y['lab_coherent'][batch_id]))
-
-        axes[1].set_title("Grayscale")
-        axes[1].imshow(squeeze(batch_x[batch_id]), cmap='gray')
-
-        axes[2].set_title("Colorful Prediction")
-        axes[2].imshow(lab2rgb(prediction[0][batch_id]))
-
-        axes[3].set_title("Coherent Prediction")
-        axes[3].imshow(lab2rgb(prediction[1][batch_id]))
-
-        plt.show()
+    for _ in range(10):
+        show_predictions(test_generator, model)
 
 
 if __name__ == "__main__":
